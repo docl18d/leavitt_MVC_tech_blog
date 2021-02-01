0c2344a762e3c01945500bbe90cff02ccb71e1fe
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-02-01 00:12:30 -0700

    fixed comment duplicate

--- seeds/post_seeds.js
+++ seeds/post_seeds.js
@@ -2,7 +2,7 @@ const { Post } = require('../models');
 
 const postData = [{
         name: 'Bjorn Longship',
-        description: 'Axes lack the table axes ikea bjorn the chair Beowulf bjorn the chair viking ikea, Leif Erikson loot horns loot. Berserker bjorn the chair ocean, swords scandinavia tall blond women sea longship berserker ocean boats swords bjorn the chair viking. Lack the table the kracken skald sea berserker longship Beowulf horns. Longship sea the kracken bork bork bork ocean scandinavia the kracken lack the table axes. Bork bork bork skald ocean tall blond women the kracken longship, terror boats horns sailing berserker ikea skald.',
+        description: "Viking boats the kracken lack the table viking ocean scandinavia swords berserker. Swords loot axes tall blond women, scandinavia boats berserker raiding berserker terror viking viking scandinavia bjorn the chair.",
         user_id: 1
 
     },
