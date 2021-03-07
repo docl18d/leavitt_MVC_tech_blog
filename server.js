52d3968c18537b4b57e7f4d65b3ba35a63c09ff1
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-03-07 14:40:57 -0700

    changed server.js and signup files

--- server.js
+++ server.js
@@ -1,42 +1,33 @@
-const path = require('path');
+onst path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require("./utils/helpers");
+const compression = require('compression');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
-const PORT = process.env.PORT || 3003;
-
-// Set up Handlebars.js engine with custom helpers
+const PORT = process.env.PORT || 3001;
+const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
-
 const sess = {
-  secret: "the key to being awesome",
+  secret: process.env.secret,
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
-
 app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
+app.use(compression());
+app.engine('handlebars', hbs.engine, 'html', require('ejs').renderFile);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now Listening to port: " + PORT));
+  app.listen(PORT, () => console.log(`Now listening at: http://localhost:${PORT}`));
 });
-
-// Heroku address https://leavitt-mvc-tech-blog.herokuapp.com/
